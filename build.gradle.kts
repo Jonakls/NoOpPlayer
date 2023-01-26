fbf747afecafb28690d96d7c5605fcccd5f1e1f2
Jonathan Narvaez <89610430+jonakls@users.noreply.github.com>
2023-01-26 02:03:02 -0500

    build(versions): add apis and libraries for project functionally with catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,52 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.21"
+    `java-library`
+    alias(libs.plugins.minecrell)
 }
-
-group = "me.jonakls"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    compileOnly(libs.spigot)
+    compileOnly(libs.vault)
+}
+
+tasks {
+
+    jar {
+        archiveBaseName.set("${rootProject.name}-v${project.version}")
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+    }
+}
+
+bukkit {
+    main = "${rootProject.group}.noopplayer.NoopPlayer"
+    name = rootProject.name
+    version = "${rootProject.version}"
+    author = "Jonakls"
+    description = "A plugin that does nothing"
+    website = "www.github.com/jonakls/no-op-player"
+    depend = listOf("Vault")
+    loadBefore = listOf("Vault")
+
+    commands {
+        register("noopplayer") {
+            description = "Main command of plugin"
+            aliases = listOf("npp")
+            permission = "noopplayer.command"
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
